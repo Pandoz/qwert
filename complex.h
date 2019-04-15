4f81c15c711f7f75c94f182e5a21623d87071c02
Kr4s4vcheGG <47522796+Pandoz@users.noreply.github.com>
2019-04-15 11:01:30 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -1,49 +1,13 @@
 #ifndef Complex_h
-
 #define Complex_h
-
-
-
 struct Complex
-
-
-
 {
-
-
-
 	double re;
-
 	double im;
-
-
-
 };
-
-
-
 bool check(double x, double y);
-
-
-
-
-
 void pluss(Complex Z, Complex X);
-
-
-
 void substruct(Complex Z, Complex X);
-
-
-
 void multiplication(Complex Z, Complex X);
-
-
-
 void devide(Complex Z, Complex X);
-
-
-
-
-
 #endif
